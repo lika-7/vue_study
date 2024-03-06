b28eae38ba291e1a275ff00404117767c15db968
lika-7 <dhwhdtn93@gmail.com>
2024-03-06 17:26:04 +0900

    SPA 기본 골격 만들기

---
+++ src/router/modules/pageRouter.js
@@ -0,0 +1,15 @@
+const pageRouter = {
+    path: "/",/*컴포넌트 경로*/
+    name: "layout",/*컴포넌트 이름*/
+    component: () => import("@/layout/index.vue"),/*컴포넌트 위치*/
+    redirect: "/main",
+    children: [
+        {
+            path: "/main",
+            name: "main",
+            component: () => import("@/views/main.vue"),
+        },
+    ],
+};
+
+export default pageRouter;
